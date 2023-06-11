d6f0f7d30f9c584ab602b66ddad60e25bac0a836
Cuan Leo <cuanleo@gmail.com>
2023-06-11 09:25:59 +0100

    add overwrite to move records into terraform state

--- personal_domain/main.tf
+++ personal_domain/main.tf
@@ -1,99 +1,109 @@
 # A Records 
 
 resource "cloudflare_record" "a_record_1" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.a_record_1_name
-  value   = var.a_record_1_value
-  type    = "A"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.a_record_1_name
+  value           = var.a_record_1_value
+  type            = "A"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "a_record_2" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.a_record_2_name
-  value   = var.a_record_2_value
-  type    = "A"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.a_record_2_name
+  value           = var.a_record_2_value
+  type            = "A"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "a_record_3" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.a_record_3_name
-  value   = var.a_record_3_value
-  type    = "A"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.a_record_3_name
+  value           = var.a_record_3_value
+  type            = "A"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "a_record_4" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.a_record_4_name
-  value   = var.a_record_4_value
-  type    = "A"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.a_record_4_name
+  value           = var.a_record_4_value
+  type            = "A"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "a_record_5" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.a_record_5_name
-  value   = var.a_record_5_value
-  type    = "A"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.a_record_5_name
+  value           = var.a_record_5_value
+  type            = "A"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "a_record_6" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.a_record_6_name
-  value   = var.a_record_6_value
-  type    = "A"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.a_record_6_name
+  value           = var.a_record_6_value
+  type            = "A"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 # CNAME Records
 
 resource "cloudflare_record" "cname_record_1" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.cname_record_1_name
-  value   = var.cname_record_1_value
-  type    = "CNAME"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.cname_record_1_name
+  value           = var.cname_record_1_value
+  type            = "CNAME"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "cname_record_2" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.cname_record_2_name
-  value   = var.cname_record_2_value
-  type    = "CNAME"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.cname_record_2_name
+  value           = var.cname_record_2_value
+  type            = "CNAME"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 # MX Records
 
 resource "cloudflare_record" "mx_record_1" {
-  zone_id  = var.cloudflare_zone_id
-  name     = var.mx_record_1_name
-  value    = var.mx_record_1_value
-  priority = "10"
-  type     = "MX"
-  ttl      = 3600
-  comment  = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.mx_record_1_name
+  value           = var.mx_record_1_value
+  priority        = "10"
+  type            = "MX"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 # SRV Records
 
 resource "cloudflare_record" "srv_record_1" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.srv_record_1_name
-  type    = "SRV"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.srv_record_1_name
+  type            = "SRV"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 
   data {
     service  = var.srv_record_1_service
@@ -107,11 +117,12 @@ resource "cloudflare_record" "srv_record_1" {
 }
 
 resource "cloudflare_record" "srv_record_2" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.srv_record_2_name
-  type    = "SRV"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.srv_record_2_name
+  type            = "SRV"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 
   data {
     service  = var.srv_record_2_service
@@ -125,11 +136,12 @@ resource "cloudflare_record" "srv_record_2" {
 }
 
 resource "cloudflare_record" "srv_record_3" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.srv_record_3_name
-  type    = "SRV"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.srv_record_3_name
+  type            = "SRV"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 
   data {
     service  = var.srv_record_3_service
@@ -145,28 +157,31 @@ resource "cloudflare_record" "srv_record_3" {
 # TXT Records
 
 resource "cloudflare_record" "txt_record_1" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.txt_record_1_name
-  value   = var.txt_record_1_value
-  type    = "TXT"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.txt_record_1_name
+  value           = var.txt_record_1_value
+  type            = "TXT"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "txt_record_2" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.txt_record_2_name
-  value   = var.txt_record_2_value
-  type    = "TXT"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.txt_record_2_name
+  value           = var.txt_record_2_value
+  type            = "TXT"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
 
 resource "cloudflare_record" "txt_record_3" {
-  zone_id = var.cloudflare_zone_id
-  name    = var.txt_record_3_name
-  value   = var.txt_record_3_value
-  type    = "TXT"
-  ttl     = 3600
-  comment = local.comment
+  zone_id         = var.cloudflare_zone_id
+  name            = var.txt_record_3_name
+  value           = var.txt_record_3_value
+  type            = "TXT"
+  ttl             = 3600
+  comment         = local.comment
+  allow_overwrite = "true"
 }
